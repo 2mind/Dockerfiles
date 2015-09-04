2ebd57324106dad8a9d954915b7bbb97f9d6a8c1
shs <shs@overstack.ru>
2015-09-05 00:43:12 +0300

    con

--- base/alpine/docker-entrypoint.sh
+++ base/alpine/docker-entrypoint.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 set -e
 
-export IP=$(ping -c1 -t1 -W0 $RN 2>&1 | tr -d '():' | awk '/^PING/{print $3}')
+#export IP=$(ping -c1 -t1 -W0 $RN 2>&1 | tr -d '():' | awk '/^PING/{print $3}')
 
-#exec "$@"
-exec bash
+exec "$@"
+
